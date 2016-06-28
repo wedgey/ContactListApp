01cc6ae6e18f326e5095332bef3660ae65808c0b
wedgey <desmondpoon5@hotmail.com>
2016-06-28 06:55:51 +0000

    Added id's and phone numbers

--- contact.rb
+++ contact.rb
@@ -4,13 +4,24 @@ require 'csv'
 # The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
+  attr_reader :id, :numbers
   attr_accessor :name, :email
   
   # Creates a new contact object
   # @param name [String] The contact's name
   # @param email [String] The contact's email address
-  def initialize(name, email)
+  def initialize(name, email, id)
     # TODO: Assign parameter values to instance variables.
+    @name = name
+    @email = email
+    @id = id
+    @numbers = []
+  end
+
+  # Adds a number to a contact
+  # @param number [PhoneNumber] The PhoneNumber object holding a number and its label
+  def add_number(number)
+    self.numbers << number
   end
 
   # Provides functionality for managing contacts in the csv file.
@@ -20,13 +31,31 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects
     def all
       # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
+      contacts = []
+      CSV.foreach('contacts.csv') do |contact|
+        contacts << Contact.new(contact[1], contact[2], contact[0])
+      end
+      contacts
     end
 
     # Creates a new contact, adding it to the csv file, returning the new contact.
     # @param name [String] the new contact's name
     # @param email [String] the contact's email
-    def create(name, email)
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
+    def create(name, email, numbers = [])
+      exists = Contact.search_by_email(email)
+      return ['exist', Contact.search_by_email(email)] if exists
+      contact = Contact.new(name, email, (self.all.count + 1))
+      numbers.each do |number|
+        contact.add_number(number)
+      end
+      CSV.open('contacts.csv', 'a') do |csv_file|
+        nums = []
+        contact.numbers.each do |number|
+          nums << "#{number.to_s}"
+        end
+        csv_file.puts [contact.id, contact.name, contact.email] + nums
+      end
+      contact
     end
     
     # Find the Contact in the 'contacts.csv' file with the matching id.
@@ -34,6 +63,10 @@ class Contact
     # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
     def find(id)
       # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
+      CSV.foreach('contacts.csv') do |contact|
+        return Contact.new(contact[1],contact[2],contact[0]) if contact[0].to_i == id
+      end
+      nil
     end
     
     # Search for contacts by either name or email.
@@ -41,6 +74,22 @@ class Contact
     # @return [Array<Contact>] Array of Contact objects.
     def search(term)
       # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      contacts = []
+      CSV.foreach('contacts.csv') do |contact|
+        if contact[1].match(/.*#{term}.*/) or contact[2].match(/.*#{term}.*/)
+          contacts << Contact.new(contact[1],contact[2],contact[0])
+        end
+      end
+      contacts
+    end
+
+    # Determines if the same email is already in the database.
+    # @param term [String] an email to search for
+    # @return [Contact, nil] with the contact with that email
+    def search_by_email(email)
+      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
+      contact = CSV.read('contacts.csv').detect() { |contact| contact[2] == email }
+      contact ? Contact.new(contact[1],contact[2],contact[0]) : nil
     end
 
   end
--- contact_list.rb
+++ contact_list.rb
@@ -1,8 +1,90 @@
 require_relative 'contact'
-
+require_relative 'phone_number'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
   # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  def initialize
+    return list_commands if ARGV.length == 0
+    case ARGV[0]
+    when 'new'
+      new_contact
+    when 'list'
+      list_contacts
+    when 'show'
+      id = ARGV[1].to_i
+      id.is_a?(Integer) && id != 0 ? show_contact(id) : (puts "Please include the id of the contact.")
+    when 'search'
+      # TODO: Implement searching for a contact
+      search_contact(ARGV[1])
+    else
+      puts "That is an invalid command."
+    end
+  end
 
+  def list_commands
+    puts "Here is a list of availabled commands:"
+    puts "\tnew\t- Create a new contact"
+    puts "\tlist\t - List all contacts"
+    puts "\tshow\t - Show a contact"
+    puts "\tsearch\t - Search contacts"
+  end
+
+  def get_numbers(name)
+    puts "Would you like to add a phone number for #{name}? y/n"
+    phone_verify = STDIN.gets.strip.downcase
+    numbers = []
+    while phone_verify == 'y'
+      puts "Please give us a label for this number."
+      label = STDIN.gets.strip
+      puts "What number would you like to add for #{name}'s #{label} number?"
+      number = STDIN.gets.strip
+      numbers << PhoneNumber.new(label, number)
+      puts "Would you like to add another phone number for #{name}?"
+      phone_verify = STDIN.gets.strip.downcase
+    end
+    numbers
+  end
+
+  def new_contact
+    puts "What is the full name of the new contact you'd like to add?"
+    name = STDIN.gets.strip
+    puts "What is #{name}'s email address?"
+    email = STDIN.gets.strip
+    numbers = get_numbers(name)
+
+    contact = Contact.create(name, email, numbers)
+    return puts "This email has already been added under #{contact[1].id}: #{contact[1].name}" if contact.is_a? Array
+    puts "#{contact.id}, #{contact.name} has been added to your list."
+  end
+
+  def list_contacts
+    count = 0
+    Contact.all.each do |contact|
+      puts "#{contact.id}: #{contact.name} (#{contact.email})"
+      count += 1
+    end
+    puts "---\n#{count} records total"
+  end
+
+  def show_contact(id)
+    contact = Contact.find(id)
+    if contact.nil?
+      puts "Contact with #{id} was not found..."
+    else
+      puts "Contact Id: #{contact.id}"
+      puts "Contact Name: #{contact.name}"
+      puts "Contact Email: #{contact.email}"
+    end
+  end
+
+  def search_contact(term)
+    contacts = Contact.search(term).each do |contact|
+      puts "#{contact.id}: #{contact.name} (#{contact.email})"
+    end
+    puts "---"
+    puts contacts.count > 1 ? "#{contacts.count} records total" : "#{contacts.count} record total"
+  end
 end
+
+app = ContactList.new
